2c203f49e940c8ea712e5514626105efb83b6a51
Yuyeon-Kim <71203852+Yuyeon-Kim@users.noreply.github.com>
2022-05-17 16:36:14 +0900

    모듈 파일명 변경, 전처리 함수 추가, 통합 파일 규칙에 맞게 클래스 명, 메서드 명 변경

--- depmodule.py
+++ depmodule.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import numpy as np
-import PIL.Image as pil
+# import PIL.Image as pil
 import matplotlib.pyplot as plt
 import cv2
 
@@ -12,7 +12,7 @@ from torchvision import transforms
 import networks
 from utils import download_model_if_doesnt_exist
 
-class DepthPrediction:
+class DepModel :
     def __init__(self) -> None:
         pass
 
@@ -36,19 +36,21 @@ class DepthPrediction:
         self.depth_decoder.eval()
 
 
-    def pil_load_image(self, image_path = "assets/human_road_image.jpg") -> np.ndarray:
-        self.input_image = pil.open(image_path).convert('RGB')
-        self.original_width, self.original_height = self.input_image.size
+    # def pil_load_image(self, image_path = "assets/human_road_image.jpg") -> np.ndarray:
+    #     self.input_image = pil.open(image_path).convert('RGB')
+    #     self.original_width, self.original_height = self.input_image.size
 
-        feed_height = self.loaded_dict_enc['height']
-        feed_width = self.loaded_dict_enc['width']
-        input_image_resized = self.input_image.resize((feed_width, feed_height), pil.LANCZOS)
-        return input_image_resized
-        
+    #     feed_height = self.loaded_dict_enc['height']
+    #     feed_width = self.loaded_dict_enc['width']
+    #     input_image_resized = self.input_image.resize((feed_width, feed_height), pil.LANCZOS)
+    #     return input_image_resized
 
-    def cv_load_image(self, image_path = "assets/human_road_image.jpg") -> np.ndarray:
+    def load_image(self, image_path = "assets/test_image.jpg") -> np.ndarray:
         self.input_image = cv2.imread(image_path)
-        self.input_image = cv2.cvtColor(self.input_image, cv2.COLOR_BGR2RGB)
+        return self.preprocess_image(self.input_image)
+
+    def preprocess_image(self, image : np.ndarray) -> np.ndarray:
+        self.input_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         self.original_height = self.input_image.shape[0]
         self.original_width = self.input_image.shape[1]
 
@@ -81,21 +83,14 @@ class DepthPrediction:
         vmax = np.percentile(self.disp_resized_np, vmax_percentage) # 백분위 percentage% 수
 
         plt.figure(figsize=(10, 10))
-        plt.subplot(211)
+        plt.subplot(121)
         plt.imshow(self.input_image)
         plt.title("Input", fontsize=22)
         plt.axis('off')
 
-        plt.subplot(212)
+        plt.subplot(122)
         plt.imshow(self.disp_resized_np, cmap='magma', vmax=vmax)
-        plt.title("Disparity prediction", fontsize=22)
+        plt.title("Depth prediction", fontsize=22)
         plt.axis('off')
 
         plt.show()
-
-# 아래는 테스트용 실행 코드
-# c = DepthPrediction()
-# c.load_model(model_name  = "mono_640x192")
-# c.cv_load_image("assets/human_road_image.jpg")
-# c.predict(c.cv_load_image())
-# c.show(vmax_percentage = 99)
