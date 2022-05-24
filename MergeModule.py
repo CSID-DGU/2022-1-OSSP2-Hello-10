3b2b6021331a76436d16191a6c77527517153715
Yuyeon-Kim <1rladbdus@gmail.com>
2022-05-24 12:29:44 +0900

    정보 종합 모듈 틀 업로드

---
+++ MergeModule.py
@@ -0,0 +1,12 @@
+class MergeModule:
+  def __init__(self):
+    pass
+
+  def current_road(self): # 현재 나의 도로
+    pass
+
+  def dep_road(self): # 거리 별 거리
+    pass
+  
+  def dep_objects(self): # 장애물 별 거리
+    pass
