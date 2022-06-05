8aacf8fb7e7a8ee439de34f434a41984c069f6a3
Kim TaeKeoum <tae3734@naver.com>
2022-06-05 20:59:31 +0900

    update

--- total_process_multi.py
+++ total_process_multi.py
@@ -1,7 +1,7 @@
 import cv2
-from odmodule.odmodule import odmodule
+from odmodule.odmodule import OdModel
 from depmodule.depmodule import DepModel
-from segmodule.segmodule import segmodule
+from segmodule import segmodule
 from mergemodule.mergemodule import MergeModule
 from alarmmodule.alarmmodule import Alarm
 from calculate.calculate import Data
@@ -9,8 +9,8 @@ from threading import Thread
 
 
 def od_pred(id, img):
-    global object_class, object_location, size, OdModel
-    od_outputs, _ = OdModel.predict(img)
+    global object_class, object_location, size, OdModule
+    od_outputs, _ = OdModule.predict(img)
     object_class = od_outputs['instances'].pred_classes.numpy()
     size = od_outputs['instances'].image_size
     object_location = od_outputs['instances'].pred_boxes.tensor.numpy()
@@ -18,24 +18,24 @@ def od_pred(id, img):
 
 
 def seg_pred(id, img):
-    global class_segmap, SegModel
-    segmap, _ = SegModel.predict(img)
+    global class_segmap, SegModule
+    segmap, _ = SegModule.predict(img)
     class_segmap = segmodule.convert(segmap)
 
 
 def dep_pred(id, img):
-    global distance, depmodel
-    image = depmodel.preprocess_image(img)
-    distance = depmodel.predict(img)
+    global distance, DepModule
+    image = DepModule.preprocess_image(img)
+    distance = DepModule.predict(img)
 
 
-OdModel = odmodule.OdModel()
-SegModel = segmodule.SegModule()
-depmodel = DepModel()
-depmodel.load_model(model_name="mono_640x192")
-mgmodule = MergeModule()
-cacmodule = Data()
-armodule = Alarm()
+OdModule = OdModel()
+SegModule = segmodule.SegModule()
+DepModule = DepModel()
+DepModule.load_model(model_name  = "mono_640x192")
+MgModule = MergeModule()
+CacModule = Data()
+ArModule = Alarm()
 
 cap = cv2.VideoCapture(1)
 
@@ -60,14 +60,14 @@ while(True):
     th2.join()
     th3.join()
 
-    mgmodule.current_road(class_segmap)
-    cur_road = mgmodule.now_road
-    dep_road_res = mgmodule.dep_road(class_segmap, distance)
-    od_classes, res = mgmodule.dep_objects(
+    MgModule.current_road(class_segmap)
+    cur_road = MgModule.now_road
+    dep_road_res = MgModule.dep_road(class_segmap, distance)
+    od_classes, res = MgModule.dep_objects(
         object_class, object_location, distance)
-    od_location = mgmodule.loc_object(size, object_location)
+    od_location = MgModule.loc_object(size, object_location)
 
-    classes, direction = cacmodule.return_highest_danger(
+    classes, direction = CacModule.return_highest_danger(
         od_classes, od_location, res, dep_road_res, cur_road)
 
-    armodule.runmodule(classes, direction)
+    ArModule.runmodule(classes, direction)
