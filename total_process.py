0d3175b3977f13c8438eb6509da8435d2af99d60
Kim TaeKeoum <tae3734@naver.com>
2022-06-05 23:18:25 +0900

    total_process

--- total_process.py
+++ total_process.py
@@ -5,6 +5,7 @@ from segmodule import segmodule
 from mergemodule.mergemodule import MergeModule
 from alarmmodule.alarmmodule import Alarm
 from calculate.calculate import Data
+import time
 
 OdModule = OdModel()
 SegModule = segmodule.SegModule()
@@ -14,30 +15,29 @@ MgModule = MergeModule()
 CacModule = Data()
 ArModule = Alarm()
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture('street3.avi')
 
 while(True):
-
     ret, image = cap.read()
 
-    od_outputs, res = OdModule.predict(image)
+    od_outputs, od_res = OdModule.predict(image)
     object_class = od_outputs['instances'].pred_classes.numpy()
     size = od_outputs['instances'].image_size
     objcet_location = od_outputs['instances'].pred_boxes.tensor.numpy()
     objcet_location = objcet_location.astype(int)
 
-    segmap, res = SegModule.predict(image)
+    segmap, seg_res = SegModule.predict(image)
     class_seg_map = segmodule.convert(segmap)
 
-    image = DepModule.preprocess_image(image)
-    distance = DepModule.predict(image)
+    pre_image = DepModule.preprocess_image(image)
+    distance = DepModule.predict(pre_image)
 
     MgModule.current_road(class_seg_map)
     cur_road = MgModule.now_road
     dep_road_res = MgModule.dep_road(class_seg_map, distance)
-    od_classes, res = MgModule.dep_objects(object_class, objcet_location, distance)
+    od_classes, dep_obj_res = MgModule.dep_objects(object_class, objcet_location, distance)
     od_location = MgModule.loc_object(size, objcet_location)
 
-    classes, direction = CacModule.return_highest_danger(od_classes, od_location, res, dep_road_res, cur_road)
+    classes, direction = CacModule.return_highest_danger(od_classes, od_location, dep_obj_res, dep_road_res, cur_road)
 
     ArModule.runmodule(classes, direction)
