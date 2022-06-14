16eef397d68850fab1fe5e7989b370da6c1e2812
Kim TaeKeoum <tae3734@naver.com>
2022-06-14 11:28:54 +0900

    total_process_multi

--- total_process_multi.py
+++ total_process_multi.py
@@ -9,24 +9,36 @@ from threading import Thread
 
 
 def od_pred(id, img):
+    print("장애물 인식 모듈 Loaded")
     global object_class, object_location, size, OdModule
     od_outputs, _ = OdModule.predict(img)
     object_class = od_outputs['instances'].pred_classes.cpu().numpy()
     size = od_outputs['instances'].image_size
     object_location = od_outputs['instances'].pred_boxes.tensor.cpu().numpy()
     object_location = object_location.astype(int)
+    print("장애물 인식 모듈 Finished")
 
 
 def seg_pred(id, img):
+    print("도로 인식 모듈 Loaded")
     global class_segmap, SegModule
     segmap, _ = SegModule.predict(img)
     class_segmap = segmodule.convert(segmap)
+    print("도로 인식 모듈 Finished")
 
 
 def dep_pred(id, img):
+    print("거리 예측 모듈 Loaded")
     global distance, DepModule
     image = DepModule.preprocess_image(img)
     distance = DepModule.predict(image)
+    print("거리 예측 모듈 Finished")
+
+def exe_alarm(id, classes, direction):
+    print("알람 모듈 Loaded")
+    global ArModule
+    ArModule.runmodule(classes, direction)
+    print("알람 모듈 Finished")
 
 
 OdModule = OdModel()
@@ -60,14 +72,19 @@ while(True):
     th2.join()
     th3.join()
 
+    print("정보 종합 모듈 Loaded")
     MgModule.current_road(class_segmap)
     cur_road = MgModule.now_road
     dep_road_res = MgModule.dep_road(class_segmap, distance)
     od_classes, res = MgModule.dep_objects(
         object_class, object_location, distance)
     od_location = MgModule.loc_object(size, object_location)
+    print("정보 종합 모듈 Finished")
 
+    print("위험도 계산 모듈 Loaded")
     classes, direction = CacModule.return_highest_danger(
         od_classes, od_location, res, dep_road_res, cur_road)
+    print("위험도 계산 모듈 Finished")
 
-    ArModule.runmodule(classes, direction)
+    th4 = Thread(target=exe_alarm, args=(4, classes, direction))
+    th4.start()
