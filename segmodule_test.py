4a2ed340018b0cfafcbdb201d54ee2957dd28ffb
Yuyeon-Kim <71203852+Yuyeon-Kim@users.noreply.github.com>
2022-06-16 01:43:29 +0900

    도로 인식 테스트 코드 최신화

--- segmodule_test.py
+++ segmodule_test.py
@@ -1,11 +1,14 @@
-from segmodule.segmodule import segmodule
+from segmodule import segmodule
 import cv2
 
-SegModel = segmodule.SegModel()
+SegModel = segmodule.SegModule()
 
 # segmap = segmentation info, res = segmap + orginal img
-segmap, res = SegModel.predict("file_path")
-# im = cv2.imread("img_file")
+segmap, res = SegModel.predict("segmodule/test_image.jpg")
+# im = cv2.imread("segmodule/test_image.jpg")
 # segmap, res = SegModel.predict(im)
 
 class_seg_map = segmodule.convert(segmap)  # (720, 1280) 각 픽셀별 클래스 값
+
+print(res)
+print(class_seg_map)
