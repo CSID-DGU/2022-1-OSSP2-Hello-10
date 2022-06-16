d12396e50143df47787e5c7a015f677f28b7af77
Kim TaeKeoum <tae3734@naver.com>
2022-06-16 15:31:28 +0900

    total_process_multi_new update

--- total_process.py
+++ total_process.py
@@ -7,6 +7,10 @@ from mergemodule.mergemodule import MergeModule
 from alarmmodule.alarmmodule import Alarm
 from calculate.calculate import Data
 import time
+import copy
+
+NUMBER_OUT_SPEACH = 2
+VISUALIZE = True
 
 OdModule = OdModel()
 print("장애물 인식 모듈 Loaded")
@@ -23,6 +27,9 @@ ArModule = Alarm()
 print("알람 모듈 Loaded")
 
 cap = cv2.VideoCapture("street3.avi")
+# cap = cv2.VideoCapture(1)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while(True):
     ret, image = cap.read()
@@ -34,39 +41,76 @@ while(True):
     object_location = object_location.astype(int)
     print("장애물 인식 모듈 Finished")
 
-    segmap, seg_res = SegModule.predict(image)
-    class_seg_map = segmodule.convert(segmap)
+    segmap, color_segmap = SegModule.predict(image)
+    class_segmap = segmodule.convert(copy.copy(segmap))
     print("도로 인식 모듈 Finished")
 
     pre_image = DepModule.preprocess_image(image)
     distance = DepModule.predict(pre_image)
     print("거리 예측 모듈 Finished")
 
-    MgModule.current_road(class_seg_map)
+    MgModule.current_road(class_segmap)
     cur_road = MgModule.now_road
-    dep_road_res = MgModule.dep_road(class_seg_map, distance)
+    dep_road_res = MgModule.dep_road(class_segmap, distance)
     od_classes, res = MgModule.dep_objects(object_class, object_location, distance)
     od_location = MgModule.loc_object(size, object_location)
     print("정보 종합 모듈 Finished")
 
-    num_detect = int(input('한 프레임당 탐색 개체 수 : '))
+    num_detect = NUMBER_OUT_SPEACH  # int(input('한 프레임당 탐색 개체 수 : '))
 
-    calculated_danger = np.array(CacModule.return_highest_danger(od_classes, od_location, res, dep_road_res, cur_road, num_detect))
-    classes, direction, order = calculated_danger[:, 0], calculated_danger[:, 1], calculated_danger[:, 2]
+    calculated_danger = np.array(CacModule.return_highest_danger(
+        od_classes, od_location, res, dep_road_res, cur_road, num_detect))
+    if calculated_danger.size !=0:
+        classes, direction, order = calculated_danger[:,0], calculated_danger[:, 1], calculated_danger[:, 2]
     print("위험도 계산 모듈 Finished")
 
-    org_image = image.copy()
-    num = classes.size
-    for i in range(num):
-        if classes[i] == -1 or classes[i] == -2:
-            ArModule.runmodule(classes[i], direction[i])
-            # 도로 시각화
-        else:
-            res_image = cv2.rectangle(image, (object_location[order[i]][0], object_location[order[i]][1]),
-                                    (object_location[order[i]][2], object_location[order[i]][3]), (0, 0, 255), 2)
-            ArModule.runmodule(classes[i], direction[i])
-            cv2.imshow("result", image)
-            cv2.waitKey(1000)
-            cv2.destroyAllWindows()
-            image = org_image.copy()
-    print("알람 모듈 Finished")
+    if type(image) == np.ndarray:  # 값이 들어왔으면
+        res_image = None
+        org_image = image.copy()
+        num = classes.size
+        for i in range(num):
+            if classes[i] == -1 or classes[i] == -2:
+                ArModule.runmodule(classes[i], direction[i])
+                if VISUALIZE and classes[i] == -1:
+                    res_image = image
+                # 도로 시각화
+                if VISUALIZE and classes[i] == -2:  # 횡단보도 시각화
+                    seg_out = copy.copy(segmap)
+                    for c in segmodule.CUSTOM_COLOR_MAP:
+                        if c != segmodule.CUSTOM_COLOR_MAP[3]:
+                            seg_out[(seg_out == c).all(axis=2)] = [0, 0, 0]
+
+                        h, w, _ = np.array(seg_out).shape
+                        img_resized = cv2.resize(image, (w, h))
+                        res_image = (img_resized * 0.5 +
+                                     seg_out * 0.5).astype(np.uint8)
+
+            else:
+                # 장애물 시각화
+                if VISUALIZE:
+                    res_image = cv2.rectangle(image, (object_location[order[i]][0], object_location[order[i]][1]),
+                                              (object_location[order[i]][2], object_location[order[i]][3]), (0, 0, 255), 2)
+                    seg_out = copy.copy(segmap)
+                    for c in segmodule.CUSTOM_COLOR_MAP:
+                        if c != segmodule.CUSTOM_COLOR_MAP[3]:
+                            seg_out[(seg_out == c).all(axis=2)] = [0, 0, 0]
+
+                    # h, w, _ = np.array(seg_out).shape
+                    # img_resized = cv2.resize(res_image, (w, h))
+                    # res_image = (img_resized * 0.5 +
+                    #              seg_out * 0.5).astype(np.uint8)
+                    # cv2.imshow("result", res_image)
+                    # cv2.waitKey(2000)
+                    # cv2.destroyAllWindows()
+                ArModule.runmodule(classes[i], direction[i])
+
+                image = org_image.copy()
+
+            if VISUALIZE:
+
+                resize_result = cv2.resize(res_image, (1080, 720))
+                cv2.imshow("result", resize_result)
+                cv2.waitKey(2000)
+                cv2.destroyAllWindows()
+
+        print("알람 모듈 Finished")
--- total_process_multi_new.py
+++ total_process_multi_new.py
@@ -8,9 +8,11 @@ from alarmmodule.alarmmodule import Alarm
 from calculate.calculate import Data
 from threading import Thread
 import time
+import copy
 
-NUMBER_OUT_SPEACH = 1
+NUMBER_OUT_SPEACH = 2
 VISUALIZE = True
+
 
 def od_pred(id, img):
     global object_class, object_location, size, OdModule
@@ -23,9 +25,9 @@ def od_pred(id, img):
 
 
 def seg_pred(id, img):
-    global class_segmap, color_segmap, SegModule
+    global class_segmap, color_segmap, SegModule, segmap
     segmap, color_segmap = SegModule.predict(img)
-    class_segmap = segmodule.convert(segmap)
+    class_segmap = segmodule.convert(copy.copy(segmap))
 
     print("도로 인식 모듈 Finished")
 
@@ -37,34 +39,55 @@ def dep_pred(id, img):
     print("거리 예측 모듈 Finished")
 
 
-def exe_alarm(id, image, classes, direction, order, object_location, class_segmap):
+def exe_alarm(id, image, classes, direction, order, object_location, class_segmap, segmap):
     global ArModule
 
-    if type(image) == np.ndarray: # 값이 들어왔으면
+    if type(image) == np.ndarray:  # 값이 들어왔으면
         res_image = None
         org_image = image.copy()
         num = classes.size
         for i in range(num):
             if classes[i] == -1 or classes[i] == -2:
                 ArModule.runmodule(classes[i], direction[i])
+                if VISUALIZE and classes[i] == -1:
+                    res_image = image
                 # 도로 시각화
-                if VISUALIZE and classes[i] == -2: # 횡단보도 시각화
-                    h, w, _ = np.array(image).shape
-                    res_image = cv2.resize(color_segmap, (w, h))
+                if VISUALIZE and classes[i] == -2:  # 횡단보도 시각화
+                    seg_out = copy.copy(segmap)
+                    for c in segmodule.CUSTOM_COLOR_MAP:
+                        if c != segmodule.CUSTOM_COLOR_MAP[3]:
+                            seg_out[(seg_out == c).all(axis=2)] = [0, 0, 0]
+
+                        h, w, _ = np.array(seg_out).shape
+                        img_resized = cv2.resize(image, (w, h))
+                        res_image = (img_resized * 0.5 +
+                                     seg_out * 0.5).astype(np.uint8)
 
             else:
                 # 장애물 시각화
                 if VISUALIZE:
                     res_image = cv2.rectangle(image, (object_location[order[i]][0], object_location[order[i]][1]),
-                                            (object_location[order[i]][2], object_location[order[i]][3]), (0, 0, 255), 2)
+                                              (object_location[order[i]][2], object_location[order[i]][3]), (0, 0, 255), 2)
+                    seg_out = copy.copy(segmap)
+                    for c in segmodule.CUSTOM_COLOR_MAP:
+                        if c != segmodule.CUSTOM_COLOR_MAP[3]:
+                            seg_out[(seg_out == c).all(axis=2)] = [0, 0, 0]
+
+                    # h, w, _ = np.array(seg_out).shape
+                    # img_resized = cv2.resize(res_image, (w, h))
+                    # res_image = (img_resized * 0.5 +
+                    #              seg_out * 0.5).astype(np.uint8)
                     # cv2.imshow("result", res_image)
                     # cv2.waitKey(2000)
                     # cv2.destroyAllWindows()
                 ArModule.runmodule(classes[i], direction[i])
+
                 image = org_image.copy()
-            
+
             if VISUALIZE:
-                cv2.imshow("result", res_image)
+
+                resize_result = cv2.resize(res_image, (1080, 720))
+                cv2.imshow("result", resize_result)
                 cv2.waitKey(2000)
                 cv2.destroyAllWindows()
 
@@ -85,12 +108,16 @@ print("위험도 계산 모듈 Loaded")
 ArModule = Alarm()
 print("알람 모듈 Loaded")
 
-cap = cv2.VideoCapture("street3.avi")
-image, classes, direction, order, object_location, color_segmap = None, None, None, None, None, None
+#cap = cv2.VideoCapture("street3.avi")
+cap = cv2.VideoCapture(1)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+image, classes, direction, order, object_location, color_segmap, segmap = None, None, None, None, None, None, None
 while(True):
-    
+
     th4 = Thread(target=exe_alarm, args=(
-        4, image, classes, direction, order, object_location, color_segmap))
+        4, image, classes, direction, order, object_location, color_segmap, segmap))
     # if th4.is_alive():
     th4.start()
 
@@ -102,7 +129,7 @@ while(True):
         target=od_pred, args=(1, image))
     th1.start()
 
-    class_segmap, color_segmap = None, None
+    class_segmap, color_segmap, segmap = None, None, None
     th2 = Thread(target=seg_pred, args=(2, image))
     th2.start()
 
@@ -123,13 +150,13 @@ while(True):
     od_location = MgModule.loc_object(size, object_location)
     print("정보 종합 모듈 Finished")
 
-    num_detect = NUMBER_OUT_SPEACH #  int(input('한 프레임당 탐색 개체 수 : '))
+    num_detect = NUMBER_OUT_SPEACH  # int(input('한 프레임당 탐색 개체 수 : '))
 
-    calculated_danger = np.array(CacModule.return_highest_danger(od_classes, od_location, res, dep_road_res, cur_road, num_detect))
-    classes, direction, order = calculated_danger[:, 0], calculated_danger[:, 1], calculated_danger[:, 2]
+    calculated_danger = np.array(CacModule.return_highest_danger(
+        od_classes, od_location, res, dep_road_res, cur_road, num_detect))
+    if calculated_danger.size !=0:
+        classes, direction, order = calculated_danger[:,0], calculated_danger[:, 1], calculated_danger[:, 2]
     print("위험도 계산 모듈 Finished")
-
-
 
     end = time.time()
     print(f"{end - start:.5f} sec")
